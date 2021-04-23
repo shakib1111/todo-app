26dde3ddb4c5b91cfdcf7eceecfd4e914bf6de35
shakib1111 <skshakib0000@gmail.com>
2021-04-23 16:24:21 +0530

    added an remove funtion in new li elements

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const ul=document.querySelector('ul');
+    const ul=document.querySelector('ul');
 
-const button=document.querySelector('button');
+    const button=document.querySelector('button');
 
 
-button.addEventListener('click', () => {
-const li=document.createElement('li');
-li.textContent='something new task added'
-ul.append(li);
+    button.addEventListener('click', () => {
+    const li=document.createElement('li');
+    li.textContent='something new task added'
+    ul.append(li);
 });
 
 
-const items=document.querySelectorAll('li')
+    // const items=document.querySelectorAll('li')
 
-items.forEach(item =>{
-    item.addEventListener('click', (e) =>{
-       
-        e.target.remove();
+    // items.forEach(item =>{
+    // item.addEventListener('click', (e) =>{
+    //     console.log('event in li')
+    //     e.stopPropagation();
+    //     e.target.remove();
+    // });
+
+    ul.addEventListener('click',e => {
+        // console.log('event in ul')
+        if(e.target.tagName === 'LI') 
+        {
+            e.target.remove()
+        }
     });
-});
