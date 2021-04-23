1cf1835f7137b88d041857dbb036c1b0096cbdce
shakib1111 <skshakib0000@gmail.com>
2021-04-24 01:27:08 +0530

    clear text field after submitting added

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const addform=document.querySelector('.add');
+const list=document.querySelector('.todos')
+
+const generateTemplate=todo=>{
+
+    const html=`
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="fas fa-trash delete"></i>
+</li>
+    `;
+    list.innerHTML +=html;
+};
+
+
 
 addform.addEventListener('submit' , e=>{
     e.preventDefault();
-    const todo=addform.add.value;
-    console.log(todo);
+    const todo=addform.add.value.trim();
+   if(todo.length)
+   {
+    generateTemplate(todo);
+    addform.reset();
+   }
 });
