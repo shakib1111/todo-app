da91116ca80f0fcfcdf6aaa4810e877b9cfe44cc
shakib1111 <skshakib0000@gmail.com>
2021-04-24 16:04:22 +0530

    delete functionality added to delete icons

--- app.js
+++ app.js
@@ -23,3 +23,10 @@ addform.addEventListener('submit' , e=>{
     addform.reset();
    }
 });
+
+list.addEventListener('click', e=>{
+    if(e.target.classList.contains('delete'))
+    {
+            e.target.parentElement.remove();
+    }
+});
